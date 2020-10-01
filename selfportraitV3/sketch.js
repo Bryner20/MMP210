8dd70a6a48a0a9f0433ae7fea77607713517c695
Bryner20 <34035527+Bryner20@users.noreply.github.com>
2020-10-01 19:51:32 -0400

    V3 work

--- selfportraitV3/sketch.js
+++ selfportraitV3/sketch.js
@@ -1,40 +1,43 @@
-
 var x=500;
-var y=200;
+var y=250;
 
-var lefteyeX= 400;
-var righteyeX= 340;
-var righteyeY= 340;
-var lefteyeY=400
+var eyeSize=100;
+var mouthSize=eyeSize;
+
+var faceRed=162;
+var faceGreen=108;
+var faceBlue=127;
 
 function setup() {
 	createCanvas(740, 460);
+	x=width/2
+	y=height/2
 }
 function draw() {
-	background(' lightblue');
+	background('lightblue');
+	x=mouseX
+	y=mouseY
 
-	stroke(255);
-    strokeWeight(10);
+	faceRed=mouseX
+	faceGreen=mouseY
 
-    line(mouseX, mouseY, pmouseX, Pmousey);
-    
-    fill(540, 283, 127);
-	ellipse(367, 155, 195, 195);
-    
-    fill('red');
-    x=mouseX;
-    y=mouseY;
-	circle(x-20, 150, 30);
+    //face
+    fill(faceRed, faceGreen, faceBlue);
+	ellipse(x-133, y-45, 195, 195);
+    //eyes
+    fill('red');   
+	circle(x-160, y-50, 30);
 	fill(0);
-	circle(x+40, 150, 30);
+	circle(x-110, y-50, 30);
 
-	rect(375, 230, 45, 55, 25, 5, 25, 20);
-	rect(315, 230, 45, 55, 5, 25, 25, 20);
-	rect(310, 45, 120, 50, 5, 55, 30, 20);
+	rect(x-125, y+30, 45, 55, 25, 5, 25, 20);
+	rect(x-185, y+30, 45, 55, 5, 25, 25, 20);
+	rect(x-190, y-155, 120, 50, 5, 55, 30, 20);
 
-	rect(350, 230, 35, 55, 75, 105, 10, 10);
+	rect(x-150, y+30, 35, 55, 75, 105, 10, 10);
     
-	line(365, 220, 385, 210);
-	line(270, 150, 465, 150);
+	line(x-135, y+20, x-115, y+10);
+	line(x-230, y-50, x-40, y-50);
+
 
 }
