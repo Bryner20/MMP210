2bdc86141dad1dbeca5485c311d319f6002d97d3
Bryner20 <34035527+Bryner20@users.noreply.github.com>
2020-12-03 01:36:03 -0500

    Pitch

---
+++ Pitch/sketch.js
@@ -0,0 +1,17 @@
+var currentSlide = 1;
+var totalSlide = 5;
+
+function setup() {
+    createCanvas('Navy');
+}
+
+function draw() {
+    background(51);
+
+    // slides
+    if (currentSlide = 1) {
+        //title
+        textSize(100);
+        text('A landscape planet', width/2, height/2);
+    }
+}
